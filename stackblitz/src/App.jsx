a203778b5c1adbd3c29d0f34ed420948bb9d4806
15aozzz <luiscajacurigutierrez@gmail.com>
2026-03-20 09:52:55 -0500

    Bloque A —proyecto Vite en StackBlitz verificado

---
+++ stackblitz/src/App.jsx
@@ -0,0 +1,11 @@
+import Home from './pages/Home.jsx';
+function App() {
+  return (
+    <div>
+      Restaurante
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ stackblitz/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h2>DON POLLO</h2>
+      <h3>Aqui van los platos</h3>
+    </div>
+  );
+}
+export default Home;
